d4d06a79f80bf5441ccd0a697a31eb3e6a097631
harris <goodhabitsamsam@gmail.com>
2026-04-28 21:13:57 +0800

    fix: working on loggin, config

--- src/app/run.py
+++ src/app/run.py
@@ -6,13 +6,13 @@ from app.setup.app_factory import create_ioc_container, create_web_app
 from app.setup.config.logs import configure_logging
 from app.setup.config.settings import AppSettings, load_settings
 
+
 def make_app(
     *di_providers: Provider,
     settings: AppSettings | None = None,
 ) -> FastAPI:
     """Pass providers to override exsiting ones for testing."""
     if settings is None:
-        configure_logging()
         settings = load_settings()
     configure_logging(level=settings.logs.level)
 
@@ -24,12 +24,14 @@ def make_app(
 
 if __name__ == "__main__":
     import uvicorn
+    import sys
+    loop = "uvloop" if sys.platform != "win32" else "asyncio"
 
     uvicorn.run(
         app=make_app(),
         port=8000,
-        reload=False,
-        loop="uvloop",
+        reload=True,    # Development mode
+        loop=loop,
     )
 
 
---
+++ src/app/setup/config/logs.py
@@ -0,0 +1,39 @@
+import logging
+from enum import StrEnum
+from typing import Final
+
+from pydantic import BaseModel, Field
+
+class LoggingLevel(StrEnum):
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
+
+DEFAULT_LOG_LEVEL: Final[LoggingLevel] = LoggingLevel.INFO
+
+FMT: Final[str] = (
+    "[%(asctime)s.%(msecs)03d] "
+    "[%(threadName)s] "
+    "%(funcName)20s "
+    "%(module)s:%(lineno)d "
+    "%(levelname)-8s - "
+    "%(message)s"
+)
+
+
+DATEFMT: Final[str] = "%Y-%m-%d %H:%M:%S"
+
+def configure_logging(
+    *,
+    level: LoggingLevel = DEFAULT_LOG_LEVEL,
+) -> None:
+    logging.basicConfig(
+        level=level.value,
+        format=FMT,
+        datefmt=DATEFMT,
+    )
+
+class LoggingSettings(BaseModel):
+    level: LoggingLevel = Field(alias="LEVEL")
